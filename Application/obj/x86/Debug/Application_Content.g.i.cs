11841a4eae608392f0aa718331ddb2fd26c0ec7e
Zhidenko Nikolay <dragg.ko@gmail.com>
2015-03-12 00:11:32 +0300

    Add KoAP to start window.

--- Application/obj/x86/Debug/Application_Content.g.i.cs
+++ Application/obj/x86/Debug/Application_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("protocol.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("koap.png")]
 
 
